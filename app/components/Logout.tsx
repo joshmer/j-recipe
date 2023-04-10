1679d352afce46dd2aebc363eceeac2682f7251d
Joseph Wondwosen <arthurtefera@gmail.com>
2023-04-11 00:18:39 +0300

    Improved style of logout button

--- app/components/Logout.tsx
+++ app/components/Logout.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const Logout = () => {
-  return <button onClick={() => signOut()}>Logout</button>;
+  return (
+    <button
+      className="flex items-center justify-between rounded-md min-w-fit w-32 px-4 py-2 text-sm font-bold bg-white text-slate-800 focus:outline-none shadow-md hover:text-slate-600 transition-colors duration-150"
+      onClick={() => signOut()}
+    >
+      <span>Logout</span>
+      <FaSignOutAlt className="w-4 h-4" />
+    </button>
+  );
 };
 
 export default Logout;
