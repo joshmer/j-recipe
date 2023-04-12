debeb4cbdbcf99e9286dbb60b8a9c3055349f548
Joseph Wondwosen <arthurtefera@gmail.com>
2023-04-13 02:45:35 +0300

    Add welcome text to login component

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -5,7 +5,8 @@ import { FaGoogle } from "react-icons/fa";
 
 const LoginButton = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-slate-800">
+    <div className="flex flex-col space-y-4 justify-center items-center min-h-screen bg-slate-800">
+      <h1 className="text-lg text-white font-bold">Welcome to J-Recipe</h1>
       <button
         className="flex items-center justify-between rounded-md min-w-fit w-40 px-4 py-2 text-lg font-bold bg-white text-slate-800 focus:outline-none shadow-md hover:-translate-y-0.5 hover:text-slate-600 transition-all duration-150"
         onClick={() => signIn("google")}
