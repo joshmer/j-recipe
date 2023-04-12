958e3f7e002fd6d334cb5ffdcd131028bd49bf26
Joseph Wondwosen <arthurtefera@gmail.com>
2023-04-12 23:53:02 +0300

    Add card

---
+++ app/components/Card.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+const Card = ({ children }: Props) => {
+  return (
+    <div className="flex bg-slate-900 rounded-lg p-4 min-w-[450px] h-96 shadow-lg">
+      {children}
+    </div>
+  );
+};
+
+export default Card;
